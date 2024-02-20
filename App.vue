a820f927d0cd66b0f5a93c38e44b454b109dad36
Viana05 <133653899+Viana05@users.noreply.github.com>
2024-02-20 09:42:03 +0800

    Add files via upload
    viana0220

---
+++ App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style scoped></style>
---
+++ main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import naive from 'naive-ui'
+import locale from 'element-plus/dist/locale/zh-cn'
+import App from './App.vue'
+import router from './router'
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(naive)
+app.use(ElementPlus, { locale })
+app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
